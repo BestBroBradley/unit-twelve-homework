69dd2f2f939b0519c7dd4ee3259466534f5d89ce
Allison Bradley <allison.bradley@outlook.com>
2020-01-27 18:05:41 -0700

    Added addRole functionality

--- app.js
+++ app.js
@@ -10,39 +10,119 @@ const connection = mysql.createConnection({
 
 
 const addDepartment = (data) => {
-// function to add new department
+    // function to add new department
 }
 
 const addDepartmentQs = () => {
-// function to call up list of inquirer questions to add department
+    // function to call up list of inquirer questions to add department
 }
 
 const addRole = (data) => {
-// function to add new role
+    let deptID
+    connection.query("SELECT * FROM department", (err, results) => {
+        if (err) throw err;
+        for (const result of results) {
+            if (result.name === data.whichdept) {
+                deptID = result.id
+            }
+        }
+    })
+    connection.query(`INSERT INTO role (title, salary, department, department_id) VALUES (?, ?, ?, ?)`, [data.title, data.salary, data.department, deptID], (err, results) => {
+        if (err) throw err;
+        console.log(`\nSuccessfully added ${data.title}\n`)
+        menuReturn();
+    })
 }
 
 const addRoleQs = () => {
-// functin to call up list of inquirer questions to add role
+    // function to call up list of inquirer questions to add role
+
+    const choicesArray = []
+    connection.query("SELECT * FROM department", (err, results) => {
+        if (err) throw err;
+        for (const result of results) {
+            choicesArray.push(result.name)
+        }
+        inquirer.prompt([
+            {
+                message: "Which department would you like to add a role to?",
+                name: "department",
+                type: "list",
+                choices: choicesArray,
+            },
+            {
+                message: "What role would you like to add?",
+                name: "title"
+            },
+            {
+                message: "What salary would you like to attach to this role?",
+                name: "salary"
+
+            }
+        ]).then((response) => {
+            addRole(response);
+        })
+    })
 }
 
 const addEmployee = (data) => {
-// function to add new employee
+    // function to add new employee
 }
 
 const addEmployeeQs = () => {
-// function to call of list of inquirer questions to add employee
+    // function to call of list of inquirer questions to add employee
+}
+
+const updateEmployee = () => {
+    // function to update an employee    
+}
+
+const updateEmployeeQs = () => {
+    // function to call list of inquirer questions to update employee
 }
 
 const viewDepartments = () => {
-// function to view a list of all departments
+    // function to view a list of all departments
+    connection.query("SELECT * FROM department", (err, results) => {
+        if (err) throw err;
+        console.log(`Current Department Listing:`)
+        for (const result of results) {
+            console.log(`${result.id} | ${result.name}`)
+        }
+        menuReturn();
+    })
 }
 
 const viewRoles = () => {
-// funcion to view a list of all roles
+    // funcion to view a list of all roles
+    connection.query("SELECT * FROM role", (err, results) => {
+        if (err) throw err;
+        console.log(`Current Role Listing By Department:\n`)
+        for (const result of results) {
+            console.log(`${result.id} | ${result.title}`)
+        }
+        console.log(`\n`)
+        menuReturn();
+    })
 }
 
 const viewEmployees = () => {
-// function to view a list of all employees
+    // function to view a list of all employees
+}
+
+const menuReturn = () => {
+    inquirer.prompt([
+        {
+            message: "Would you like to Return to Menu or Exit?",
+            name: "menu",
+            type: "list",
+            choices: ["Return to Menu", "Exit"]
+        }
+    ]).then(function (response) {
+        if (response.menu === "Return to Menu") {
+            mainMenu();
+        } else { exit() }
+    })
 }
 
 const exit = () => {
@@ -62,18 +142,25 @@ const mainMenu = () => {
     ]).then(answers => {
         switch (answers.userchoice) {
             case "View departments breakdown":
+                viewDepartments();
                 break;
             case "View role breakdown":
+                viewRoles();
                 break;
             case "View employee breakdown":
+                viewEmployees();
                 break;
             case "Add a department":
+                addDepartmentQs();
                 break;
             case "Add a role":
+                addRoleQs();
                 break;
             case "Add an employee":
+                addEmployeeQs();
                 break;
             case "Update an employee":
+                updateEmployeeQs();
                 break;
             case "Exit":
                 exit();
